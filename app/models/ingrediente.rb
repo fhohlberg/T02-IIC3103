b5c6da878650054da9e9798505096e8fdf4e2f15
Francisca Hohlberg <francisca@deeplegal.ai>
2020-04-27 16:49:48 -0400

    deploy1

--- app/models/ingrediente.rb
+++ app/models/ingrediente.rb
@@ -2,7 +2,7 @@ class Ingrediente < ApplicationRecord
     include Rails.application.routes.url_helpers
     
     has_many :hamburguesa_ingredientes, dependent: :destroy
-    validates :nombre, :descripcion, presence: true
+    validates :id, :nombre, :descripcion, presence: true
 
     
 end
